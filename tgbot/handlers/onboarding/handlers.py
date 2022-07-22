ed4b49d3001bec807731c0ee7dd77d9c70ec5c3c
Timur2311 <zayniddinov.programming@gmail.com>
2022-07-22 16:15:05 +0500

    ishlavotti mario

--- tgbot/handlers/onboarding/handlers.py
+++ tgbot/handlers/onboarding/handlers.py
@@ -9,14 +9,16 @@ from tgbot.handlers.utils.info import extract_user_data_from_update
 from tgbot.models import User
 from tgbot.handlers.onboarding.keyboards import make_keyboard_for_start_command
 
-
+import requests
+from telegram import ParseMode
+from bs4 import BeautifulSoup
 
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!\nMa\'lumot qidirish uchun quyida qidirayotgan narsangizni yozing!',
-        reply_markup=ForceReply(selective=True),
+    update.message.reply_text(
+        'Salom. Ma\'lumot qidirish uchun quyida qidirayotgan narsangizni yozing!',
+        
     )
 
 
@@ -26,8 +28,50 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    URL = "https://asaxiy.uz/product?key="
+    URL+=str(update.message.text)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    item_divs= soup.find_all('div', class_="product__item d-flex flex-column justify-content-between")[:10]
+    images = []
+    titles =[]
+    prices = []
+    linkes = []
+    
+    for item_div in item_divs:
+        image = item_div.find("div", class_="product__item-img").img.get('data-src')
+        
+        if image is not None:
+            if ".webp" in image:
+                images.append(image[:(len(image)-5)])
+            else:
+                images.append(image)
+                
+        else:
+            images.append("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd-y-IJN8glQlf1qoU01dEgGPUa0d1-sjfWg&usqp=CAU")
+        
+            
+            
+        
+        product_info =item_div.find("div", class_="product__item-info")
+        
+        title = product_info.a.h5.text
+        titles.append(title)
+        if product_info.a['href'] is not None:
+            link = product_info.a['href']
+            linkes.append("asaxiy.uz"+link)            
+        else:
+            link = None  
+        price = product_info.find("div", class_="product__item-info--prices").find('span',class_="product__item-price").text
+        if price is not None:
+            prices.append(price)
+        else:
+            prices.append("Narxi belgilanmagan")
+            
+            
+    print(f"titles======{len(titles)}\n\nprices====={len(prices)}\n\nimages======{len(images)}\n\nlinkes======{len(linkes)}")
+    for i in range(10):
+        update.message.reply_photo(photo=f"{images[i]}", caption=f"<a href='{linkes[i]}'> {titles[i]} </a> \n\n Price: {prices[i]}", parse_mode=ParseMode.HTML)
 
 
 
